e6d5879f0428de411c890fc76546c1b9e0a91df0
umarfataai-coder <umarfataai@gmail.com>
2026-08-28 18:35:06 +0100

    Feat: Implement FataTokenizer in model directory

--- model/tokenizer.py
+++ model/tokenizer.py
@@ -1,8 +1,24 @@
-import re
+﻿import re
+
 class FataTokenizer:
     def __init__(self):
-        self.vocab = {}
-    def encode(self, text):
-        return [ord(c) for c in text]
-    def decode(self, tokens):
-        return "".join([chr(t) for t in tokens])
+        self.vocab = {"[ PAD ]": 0, "[ UNK ]": 1, "[ BOS ]": 2, "[ EOS ]": 3}
+        self.inverse_vocab = {v: k for k, v in self.vocab.items()}
+
+    def fit(self, texts):
+        idx = len(self.vocab)
+        for text in texts:
+            tokens = re.findall(r"\w+|[^\w\s]", text.lower())
+            for token in tokens:
+                if token not in self.vocab:
+                    self.vocab[token] = idx
+                    self.inverse_vocab[idx] = token
+                    idx += 1
+
+    def encode(self, textg):
+        tokens = re.findall(r"\w+|[^\ws]", text.lower())
+        return [self.vocab.get(token, self.vocab["[ UNK ]"]) for token in tokens]
+
+    def decode(self, ids):
+        return " ".join([self.inverse_vocab.get(i, "[ UNK ]") for i in ids])
+
